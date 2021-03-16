80b8b922ef563e6e38ad57f5a6cc3b2abbacf6b9
robFraser1111 <robfraserdesign@gmail.com>
2021-03-16 19:15:36 +1100

    Updated Bio text

--- src/views/Bio.vue
+++ src/views/Bio.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="bio">
         <p>
-            From the small Aboriginal community of Woorabinda in Central Queensland comes a 21 year old with a voice ready to be heard.
+            <i>"Miiesha holds multitudes - she is a movement" - Cyclone Wehner - NME</i>
             <br><br>
-            A strong, Anangu/Torres Strait Islander woman, Miiesha (Pronounced My-ee-sha) has been singing for her family and her community since the age of 8, and has since been developing her songwriting as a teenager. Now stepping onto a bigger stage, Miiesha is sharing her music with the world.
+            <span>From the small Aboriginal community of Woorabinda Miiesha arrived on the RnB landscape with a sound and a story that instantly demanded attention.
             <br><br>
-            Inspired by the sounds of RnB, Gospel and Soul, and the power of spoken word poetry, Miiesha’s music seeks to bring people together to help educate and inspire.
+            A strong, proud Anangu/Torres Strait Islander woman, Miiesha (Pronounced My-ee-sha) released her debut collection of songs entitled Nyaaringu in 2020. Meaning 'what happened' in Pitjantjatjara language, Nyaaringu explores the stories and the strength Miiesha has inherited from her late Grandmother, whose interludes narrate the project.Since its release, Nyaaringu has not only garnered critical acclaim, but received the 2020 ARIA for Best Soul/RnB Release, a QLD Music Award, a NIMA, and went on to top multiple Album of the Year lists.
             <br><br>
-            Gifted with a voice that showcases vulnerability and strength, often in the one breath, Miiesha sings of her people and her community with the words of a leader and a teacher.
+            Inspired by the sounds of RnB, Gospel and Soul, and the power of spoken word poetry, Miiesha is using her music to bring people together to help educate and inspire.
             <br><br>
-            Having supported the likes of Baker Boy, Briggs, Adrian Eagle and Thelma Plum as well as performing at Vanfest and Laneway Festival, Miiesha is using her voice and her message to bring her community to the world.
+            With a voice that showcases vulnerability and strength, often in the one breath, Miiesha sings of her people, her community and her story with the raw emotion of lived experience.
             <br><br>
-            Miiesha hails from the small Aboriginal community of Woorabinda in Central Queensland. Since making her debut in mid-2019, Miiesha has released a string of mesmerising singles – ‘Black Privilege’, ‘Drowning’, ‘Twisting Words’ and ‘Hold Strong’. These songs later came together as part of her fearless debut album ‘Nyaaringu’, which was one of 2020’s most affecting and critically lauded albums, nominated for five ARIA awards including Best Female and Breakthrough Artist Miiesha won the Best Soul/R&amp;B ARIA award.
+            Miiesha's story is not just worth hearing, it's needed.</span>
         </p>
     </div>
 </template>
@@ -26,9 +26,9 @@ export default {
 
     .bio {
         max-width: 900px;
-        text-align: left;
         color: #ffffff;
         padding: 30px;
+        text-align: left;
         display: flex;
         justify-self: center;
         align-items: center;
