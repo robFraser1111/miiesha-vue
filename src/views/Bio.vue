23e034a9ae53e5c376fc83f19d813b0692d295af
robFraser1111 <robfraserdesign@gmail.com>
2021-03-16 19:32:30 +1100

    Updated Bio again

--- src/views/Bio.vue
+++ src/views/Bio.vue
@@ -5,7 +5,9 @@
             <br><br>
             <span>From the small Aboriginal community of Woorabinda Miiesha arrived on the RnB landscape with a sound and a story that instantly demanded attention.
             <br><br>
-            A strong, proud Anangu/Torres Strait Islander woman, Miiesha (Pronounced My-ee-sha) released her debut collection of songs entitled Nyaaringu in 2020. Meaning 'what happened' in Pitjantjatjara language, Nyaaringu explores the stories and the strength Miiesha has inherited from her late Grandmother, whose interludes narrate the project.Since its release, Nyaaringu has not only garnered critical acclaim, but received the 2020 ARIA for Best Soul/RnB Release, a QLD Music Award, a NIMA, and went on to top multiple Album of the Year lists.
+            A strong, proud Anangu/Torres Strait Islander woman, Miiesha (Pronounced My-ee-sha) released her debut collection of songs entitled Nyaaringu in 2020. Meaning 'what happened' in Pitjantjatjara language, Nyaaringu explores the stories and the strength Miiesha has inherited from her late Grandmother, whose interludes narrate the project.
+            <br><br>
+            Since its release, Nyaaringu has not only garnered critical acclaim, but received the 2020 ARIA for Best Soul/RnB Release, a QLD Music Award, a NIMA, and went on to top multiple Album of the Year lists.
             <br><br>
             Inspired by the sounds of RnB, Gospel and Soul, and the power of spoken word poetry, Miiesha is using her music to bring people together to help educate and inspire.
             <br><br>
