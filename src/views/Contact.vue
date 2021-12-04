d0277ff4b6869a281ad87e43dc9f02d694681363
robFraser1111 <robfraserdesign@gmail.com>
2021-12-04 13:09:46 +1100

    Added menu items.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,43 +1,43 @@
 <template>
     <div class="contact">
         <p>
-            Bookings<br>
+            Bookings<br />
             <a href="mailto:katie@villagesounds.com">katie@villagesounds.com</a>
-            <br><br><br>
-            Management<br>
-            <a href="mailto:info@miiesha.com">info@miiesha.com</a>
+            <br /><br /><br />
+            Management<br />
+            <a href="mailto:info@morningbelle.com.au"
+                >info@morningbelle.com.au</a
+            >
         </p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Contact'
-}
+    name: "Contact",
+};
 </script>
 
 <style>
+a {
+    color: #ffffff;
+}
 
-    a {
-        color:#ffffff;
+.contact {
+    padding: 30px;
+    display: flex;
+    justify-self: center;
+    align-items: center;
+    color: #ffffff;
+}
+
+.contact p {
+    font-size: 24px;
+}
+
+@media only screen and (max-width: 600px) {
+    #app {
+        background-position: 600px 0;
     }
-
-    .contact {
-        padding: 30px;
-        display: flex;
-        justify-self: center;
-        align-items: center;
-        color: #ffffff;
-    }
-
-    .contact p {
-        font-size: 24px;
-    }
-
-    @media only screen and (max-width: 600px) {
-        #app {
-            background-position: 600px 0;
-        }
-    }
-
+}
 </style>
